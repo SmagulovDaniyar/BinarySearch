7dbe74d424753de57d3de4aaaab8e51ae4651c09
Daniyar <Daniyar@DESKTOP-K3C1BKU>
2022-02-16 21:57:48 +0600

    Добавьте файлы проекта.

---
+++ BinarySearch/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace BinarySearch
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputNumber = new System.Windows.Forms.TextBox();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.Execute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputNumber
+            // 
+            this.InputNumber.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputNumber.Location = new System.Drawing.Point(311, 74);
+            this.InputNumber.MaxLength = 2;
+            this.InputNumber.Name = "InputNumber";
+            this.InputNumber.Size = new System.Drawing.Size(100, 23);
+            this.InputNumber.TabIndex = 0;
+            this.InputNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputNumber_KeyPress);
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputLabel.Location = new System.Drawing.Point(12, 70);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(285, 28);
+            this.InputLabel.TabIndex = 1;
+            this.InputLabel.Text = "Введите искомое число";
+            // 
+            // Execute
+            // 
+            this.Execute.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Execute.Location = new System.Drawing.Point(426, 50);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(166, 67);
+            this.Execute.TabIndex = 2;
+            this.Execute.Text = "Найти";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 153);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.InputNumber);
+            this.MaximizeBox = false;
+            this.Name = "MyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BinarySearch";
+            this.Load += new System.EventHandler(this.MyForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputNumber;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.Button Execute;
+    }
+}
+
---
+++ BinarySearch/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinarySearch
+{
+    public partial class MyForm : Form
+    {
+        int[] array = new int[30];
+        Button[] buttons = new Button[30];
+
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+            InitializeArray();
+            CreateButtons();
+        }
+
+        private void Wait(double seconds)
+        {
+            int ticks = System.Environment.TickCount + (int)Math.Round(seconds * 1000.0);
+            while (System.Environment.TickCount < ticks)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        void InitializeArray()
+        {
+            for (int i = 0; i < 10; ++i)
+                array[i] = i + 1;
+
+            for (int i = 10; i < 20; ++i)
+                array[i] = (i + 1) * 2;
+
+            for (int i = 20; i < 30; ++i)
+                array[i] = (i + 1) * 3;
+        }
+
+        void CreateButtons()
+        {
+            for (int i = 0; i < 30; ++i)
+            {
+                Button button = new Button();
+                button.Size = new Size(30, 30);
+                button.Location = new Point(30 * i, 0);
+                button.Text = Convert.ToString(array[i]);
+                Controls.Add(button);
+                buttons[i] = button;
+            }
+        }
+
+        void PaintButtons(int from, int to, String color)
+        {
+            for (int i = from; i <= to; ++i)
+            {
+                switch(color)
+                {
+                    case "Red":
+                        {
+                            buttons[i].BackColor = Color.Red;
+                            break;
+                        }
+                    case "Yellow":
+                        {
+                            buttons[i].BackColor = Color.Yellow;
+                            break;
+                        }
+                    case "Green":
+                        {
+                            buttons[i].BackColor = Color.Green;
+                            break;
+                        }
+                }
+            }
+        }
+
+        void BinarySearch(int from, int to, int key)
+        {
+            Wait(1.0);
+
+            int temp = (from + to) / 2;
+
+            if (key == array[temp])
+            {
+                PaintButtons(temp, temp, "Green");
+                return;
+            }
+
+            if (from == to)
+            {
+                PaintButtons(temp, temp, "Red");
+                return;
+            }
+
+            if (key > array[temp])
+            {
+                PaintButtons(from, temp, "Red");
+                BinarySearch(temp + 1, to, key);
+            }
+
+            if (key < array[temp])
+            {
+                PaintButtons(temp, to, "Red");
+                BinarySearch(from, temp - 1, key);
+            }
+
+            return;
+        }
+
+        void UltraMegaHaroshSearch(int from, int to, int key)
+        {
+            Wait(1.0);
+
+            int current = (from + to) / 2;
+            int temp = from + (to - from) / 3;
+
+            if (key == array[current])
+            {
+                PaintButtons(current, current, "Green");
+                return;
+            }
+
+            if (from == to)
+            {
+                PaintButtons(current, current, "Red");
+                return;
+            }
+
+            if (key < array[temp + 1])
+            {
+                PaintButtons(temp + 1, to, "Red");
+                UltraMegaHaroshSearch(from, temp, key);
+            }
+
+            if (key > array[temp] &&
+                key < array[temp * 2 + 1])
+            {
+                PaintButtons(from, temp, "Red");
+                PaintButtons(temp * 2 + 1, to, "Red");
+                UltraMegaHaroshSearch(temp + 1, temp * 2, key);
+            }
+
+            if (key > array[temp * 2])
+            {
+                PaintButtons(from, temp * 2, "Red");
+                UltraMegaHaroshSearch(temp * 2 + 1, to, key);
+            }
+
+            return;
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            if (InputNumber.TextLength == 0)
+            {
+                MessageBox.Show("Введите значение!");
+                return;
+            }
+
+            int key = Convert.ToInt32(InputNumber.Text);
+
+            PaintButtons(0, array.Length - 1, "Yellow");
+
+            //BinarySearch(0, array.Length - 1, key);
+            UltraMegaHaroshSearch(0, array.Length - 1, key);
+        }
+
+        private void InputNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
